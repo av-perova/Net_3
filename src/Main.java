ee1ffdfb02b0761b9fb7cbb049ba1bd881799a8e
perova test <av_perova@mail.com>
2023-03-24 23:10:32 +0700

    Count of miles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int cost = 12345;
+        byte rate = 20;
 
+        int miles = cost / rate;
+
+        System.out.println("Количество начисленных миль за купленный билет " + miles);
     }
 }
